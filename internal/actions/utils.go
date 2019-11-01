d337d28ddd9e68ec42e91098a6f153a73a73babf
nicolascb <ndevbarbosa@gmail.com>
2019-11-01 17:30:27 -0300

    Parse options from command
    Check err and return a map with options that were passed via argument

--- internal/actions/utils.go
+++ internal/actions/utils.go
@@ -1,6 +1,9 @@
 package actions
 
-import "strings"
+import (
+	"errors"
+	"strings"
+)
 
 func parseURI(uri string) (string, string, string) {
 	var (
@@ -26,4 +29,41 @@ func parseURI(uri string) (string, string, string) {
 	}
 
 	return user, port, hostname
+}
+
+func getHostOptions(alias, uri, sshkey string, options []string) (map[string]string, error) {
+	hostOptions := make(map[string]string)
+
+	if alias != "*" {
+		user, port, hostname := parseURI(uri)
+
+		if len(user) > 0 {
+			hostOptions["user"] = user
+		}
+
+		if len(port) > 0 {
+			hostOptions["port"] = port
+		}
+
+		if len(hostname) > 0 {
+			hostOptions["hostname"] = hostname
+		} else {
+			return nil, errors.New("Hostname not found")
+		}
+	}
+
+	if len(sshkey) > 0 {
+		hostOptions["identityfile"] = sshkey
+	}
+
+	for _, opt := range options {
+		splited := strings.Split(opt, "=")
+		if len(splited) > 1 {
+			key := strings.ToLower(splited[0])
+			val := strings.ToLower(splited[1])
+			hostOptions[key] = val
+		}
+	}
+
+	return hostOptions, nil
 }
