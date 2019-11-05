e9679c1104afc321bcead0d4dea16a14cc32cc95
Nicolas Barbosa <ndevbarbosa@gmail.com>
2019-11-04 22:05:40 -0300

    Update version to 2.0

--- internal/app/constants.go
+++ internal/app/constants.go
@@ -10,7 +10,7 @@ const (
 ╚═╝  ╚═══╝╚══════╝╚══════╝╚═╝  ╚═╝
                                   
 `
-	appVersion     = "1.0.4"
+	appVersion     = "2.0"
 	appDescription = "An easy way to manage SSH config, see more: github.com/nicolascb/nssh"
 	appUsage       = "nssh - An easy way to manage SSH config"
 	appUsageText   = "nssh [global options] command [command options] [arguments...]"
